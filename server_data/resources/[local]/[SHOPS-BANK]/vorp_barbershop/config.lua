60e5f3d0e4f7ff14af7dee2f7123b2ca0871734f
Thomas Bastin <thomas.bastin@student.hepl.be>
2022-11-02 21:52:13 +0100

    :sparkles: Translate BarberShop

--- server_data/resources/[local]/[SHOPS-BANK]/vorp_barbershop/config.lua
+++ server_data/resources/[local]/[SHOPS-BANK]/vorp_barbershop/config.lua
@@ -9,67 +9,63 @@ Config.keys = {
 	["SHIFT"] = 0x8FFC75D6,
 	["LALT"] = 0x8AAA0AD4,
 	["B"] = 0x4CC0E2FE,
-
-
 }
-
-
 
 Config.locations = { 
 	blackwater = { 
-		blip = true,
+		blip = false,
 		pos = {x = -814.915,y = -1367.89,z = 43.75},
-		price = 10,
+		price = 2.3,
 		radius = 2.0,
 	},
 	valentine = { 
-		blip = true,
+		blip = false,
 		pos = {x = -307.51,y = 813.96,z = 118.99},
-		price = 10,
+		price = 1.7,
 		radius = 1.5, -- careful with this
 	},
 	saintdenis = { 
-		blip = true,
+		blip = false,
 		pos = {x = 2655.21,y = -1179.9,z = 53.27},
-		price = 10,
+		price = 3,
 		radius = 2.0,
 	},
 	annsberg = { 
-		blip = true,
+		blip = false,
 		pos = {x = 2924.3,y = 1343.7,z = 44.4},
-		price = 10,
+		price = 4,
 		radius = 1.5,
 	},
 	armadillo = { 
-		blip = true,
+		blip = false,
 		pos = {x = -3684.22,y = -2621.05,z = -13.44},
-		price = 10,
+		price = 4.3,
 		radius = 1.5,
 	},
 	tumbleweed = { 
-		blip = true,
+		blip = false,
 		pos = {x = -5493.4,y = -2940.7,z = -0.46},
-		price = 10,
+		price = 5.5,
 		radius = 1.5,
 	},
 	strawberry = { 
-		blip = true,
+		blip = false,
 		pos = {x = -1810.92,y = -372.15,z = 162.89},
-		price = 10,
+		price = 3.3,
 		radius = 1.5,
 	},
 }
 
 
 language = {
-	openmenu = "Open Menu",
+	openmenu = "S'installez",
 	barbershop = "Barbershop",
-	hair = "Hair",
-	hair2 = "Style Your Hair",
-	beard = "Beard",
-	beard2 = "Style Your Beard",
-	confirm = "Confirm Selection",
-	confirm2 = "Price $",
-	youpaid = "Paid $",
-	nomoney = "Not Enough Cash",
+	hair = "Cheveux",
+	hair2 = "Style de cheveux",
+	beard = "Barbe",
+	beard2 = "Style de barbe",
+	confirm = "Confirmer votre Selection",
+	confirm2 = "Prix $",
+	youpaid = "Payer $",
+	nomoney = "Pas assez d'argent",
 }
